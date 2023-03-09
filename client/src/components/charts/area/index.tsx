185316f3f687e1ca1f564bf376941e0188d820f0
Saif Eldeen Essam <saifeldeenessam01@gmail.com>
2023-03-09 19:51:20 +0200

    Updated `Info` component

--- client/src/components/charts/area/index.tsx
+++ client/src/components/charts/area/index.tsx
@@ -1,7 +1,7 @@
 import { Chart as ChartJS, registerables } from "chart.js";
 import { Line } from "react-chartjs-2";
 import Selector from './../../../components/selector';
-import DevicesTypes from './../../../data/devices-types';
+import DevicesJSON from './../../../assets/jsons/devices.json';
 import './style.css';
 
 ChartJS.register(...registerables);
@@ -15,7 +15,7 @@ function AreaChart(props: Props) {
 		<div className='area-chart'>
 			<div className="area-chart-header">
 				<span>Best Selling Devices</span>
-				<Selector options={DevicesTypes} selected={DevicesTypes[0]} setter={() => { }} />
+				<Selector options={DevicesJSON.types} selected={DevicesJSON.types[0]} setter={() => { }} />
 			</div>
 			<div className="area-chart-body">
 				<Line data={props.data} options={{ plugins: { legend: { display: false } } }} />
--- client/src/components/info/index.tsx
+++ client/src/components/info/index.tsx
@@ -5,7 +5,6 @@ import Message from '../../types/message';
 import OrderType from '../../types/order-data';
 import currencyFormat from '../../utilities/currency-format';
 import Button from '../button';
-import NumberInput from '../inputs/number';
 import { ReactComponent as CartIcon } from './../../assets/svgs/icons/cart.svg';
 import './style.css';
 
@@ -16,55 +15,55 @@ interface Props {
 function Info(props: Props) {
 	const authToken = useAuthToken();
 	const [orderData, setOrderData] = useState<OrderType>({ receiver: undefined, items: [{ device: props.data._id, quantity: 1 }], status: "SAVED" });
+	const [loading, setLoading] = useState<boolean>(false);
 	const [message, setMessage] = useState<Message>({ succeed: null, response: null });
+
 
 	useEffect(() => {
 		if (authToken) setOrderData((prev) => ({ ...prev, receiver: authToken.id }));
 	}, [authToken]);
 
 	async function addtoCart(): Promise<void> {
+		setLoading(true);
 		try {
 			const options: RequestInit = { method: "POST", body: new Blob([JSON.stringify(orderData)], { type: 'application/json' }), cache: "no-store", credentials: "include" };
 			const response = await fetch("/orders/create", options);
-			await response.json().then((data: Message) => alert(data.response));
+			await response.json().then((data: Message) => setMessage(data));
 		} catch (error) {
 			console.error("Request error", error);
 		}
+		window.scrollTo(0, 0);
+		setLoading(false);
 	}
 
 	return (
 		<div className='info'>
-			<div className="info-image">
+			{message.succeed !== null && message.response !== null ? <span className={message.succeed ? "success" : "fail"}>{message.response}</span> : undefined}
+			<div className="image">
 				<img src={require(`./../../assets/images/${props.data.image}`)} alt={props.data.brand} loading="lazy" />
 			</div>
-			<div className="info-details">
-				<div className="header">
-					<span>{props.data.color} {props.data.brand} {props.data.model}</span>
+			<div className="title">
+				<span>{props.data.color} {props.data.brand} {props.data.model}</span>
+			</div>
+			<div className="sets">
+				<div className="set">
+					<span>Pice</span>
 					<span>{currencyFormat(props.data.price)}</span>
 				</div>
-				<div className="body">
-					<div className="set">
-						<span>RAM</span>
-						<span>{props.data.ram}</span>
-					</div>
-					<div className="set">
-						<span>ROM</span>
-						<span>{props.data.rom}</span>
-					</div>
-					<div className="set">
-						<span>Condition</span>
-						<span>{props.data.condition}</span>
-					</div>
-					<div className="set">
-						<span>Stock</span>
-						<span style={{ color: props.data.stock === 0 ? "#d90429" : "#0ead69" }}>{props.data.stock === 0 ? "Out of stock" : "Available"}</span>
-					</div>
+				<div className="set">
+					<span>RAM</span>
+					<span>{props.data.ram}</span>
 				</div>
-				<div className="footer">
-					<NumberInput label="Quantity" name="quantity" setter={setOrderData} />
-					<Button type="button" label={'Add to cart'} icon={<CartIcon />} disabled={props.data.stock === 0} action={addtoCart} primary small />
+				<div className="set">
+					<span>ROM</span>
+					<span>{props.data.rom}</span>
+				</div>
+				<div className="set">
+					<span>Condition</span>
+					<span>{props.data.condition}</span>
 				</div>
 			</div>
+			<Button type="button" label={loading ? 'Loading' : 'Add to cart'} disabled={props.data.stock === 0} action={addtoCart} primary />
 		</div>
 	);
 }
