26ec6359275cad0b5a22f8621485b2f3a358b39c
Saif Eldeen Essam <saifeldeenessam01@gmail.com>
2023-03-09 15:09:14 +0200

    Removed `authenticate` middleware from `announcementsRouter` get route

--- server/src/routers/announcements.ts
+++ server/src/routers/announcements.ts
@@ -8,10 +8,10 @@ import authenticate from '../middlewares/authenticate';
 
 const announcementsRouter = Router();
 
-announcementsRouter.get('/', authenticate, readAll);
+announcementsRouter.get('/', readAll);
 announcementsRouter.post('/create', authenticate, create);
 announcementsRouter.put('/update', authenticate, update);
 announcementsRouter.delete('/remove', authenticate, remove);
-announcementsRouter.get('/:id', authenticate, readOne);
+announcementsRouter.get('/:id', readOne);
 
 export default announcementsRouter;
